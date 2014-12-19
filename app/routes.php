963b859e9c1b2f2889c3ae154dc62162774c1eb8
Frank <frank.qf.cn@gmail.com>
2014-12-19 15:41:23 +0800

    laravel is imported

---
+++ app/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	// return View::make('hello');
+	return 'Hello World ==> GET';
+});
+
+Route::post('foo/bar', function()
+{
+    return 'Hello World ==> POST';
+});
