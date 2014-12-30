9ecce4971bebed0906d4ce5555226ae72975d9cc
Frank <frank.qf.cn@gmail.com>
2014-12-30 13:23:08 +0800

    fix

--- fan/app/views/order/show.blade.php
+++ fan/app/views/order/show.blade.php
@@ -64,15 +64,11 @@
 		    <tbody>
 		    	<?php $imgItem = 0;?>
 				@foreach ($images as $image)
-				@if ($imgItem%5 == 0)
-	    		<tr>
-	    		@endif
+				<?php echo $imgItem%9 == 0 ? '<tr>' : '' ;?>
 					<td  class="col-md-1"><a href="/{{ $image->path . $image->filename}}">
 						{{ HTML::image('packages/uploads/thumbnails/thumb-' . $image->filename, 'null', array('class' => 'img-rounded img-responsive')) }}
 					</a></td>
-				@if ($imgItem%5 == 0)
-				</tr>
-				@endif
+				<?php echo $imgItem%9 == 0 ? '</tr>' : '' ;?>
 				<?php $imgItem++; ?>
 				@endforeach
 			</tbody>
