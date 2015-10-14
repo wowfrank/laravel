7a7beb9ac1af066a4bdc22b51aeca755a6931754
Frank <frank.qf.cn@gmail.com>
2015-10-14 15:29:15 +0800

    try to upload netcafe to server

--- fan/app/views/includes/footer.blade.php
+++ fan/app/views/includes/footer.blade.php
@@ -1,2 +1,2 @@
 <hr />
-<div class="copyright text-right" style="padding-bottom: 20px;">&#0169Copyright  2014</div>
+<div class="copyright text-right" style="padding-bottom: 20px;">&#0169Copyright  2015</div>
