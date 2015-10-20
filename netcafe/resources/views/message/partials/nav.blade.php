4314559885837dc18fe81a76b1df90d9512d98bb
Frank <frank.qf.cn@gmail.com>
2015-10-20 10:50:19 +0800

    fix nav

--- netcafe/resources/views/message/partials/nav.blade.php
+++ netcafe/resources/views/message/partials/nav.blade.php
@@ -12,7 +12,7 @@
                         <li><a href="/">{{ trans('messages.Home') }}</a></li>
                         <li><a href="{{ route('blog.index') }}">{{ trans('messages.Blog') }}</a></li>
                         <li class="active"><a href="#">{{ trans('messages.Messages') }}</a></li>
-                        {{dd(Auth::user()->name)}}
+                        {{dd(Auth::user()->avatar)}}
                         @if(Auth::check())
                             <li>
                                 <a href="{{ Auth::logout() }}">
